0a3b38c5cf94becb11523d4d3021a8a515817465
Alvaro Tolosa <alvaro@Alvaros-MacBook-Pro.local>
2017-02-20 07:08:27 -0500

    add map size validator, the map needs to be at least 2x2 big

---
+++ src/ca/concordia/soen6441/d20/gamemap/validator/SizeValidator.java
@@ -0,0 +1,23 @@
+package ca.concordia.soen6441.d20.gamemap.validator;
+
+import ca.concordia.soen6441.d20.gamemap.GameMap;
+
+public class SizeValidator {
+	private static final int MIN_HIGHT= 2;
+	private static final int MIN_WIDTH= 2;
+private GameMap gameMap;
+	
+	public SizeValidator(GameMap gameMap) {
+		this.gameMap = gameMap;
+	}
+	
+	/**
+	 * validate the size of the map. It needs to be bigger that 2x2
+	 * @param gameMap the game map
+	 * @return true if the validation successes, false otherwise
+	 */
+	public boolean validate() {
+		return this.gameMap.getHeight()>MIN_HIGHT && this.gameMap.getWidth()>MIN_WIDTH;
+	}
+
+}
