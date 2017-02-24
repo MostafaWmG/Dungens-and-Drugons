e525b9e35784bb5bedc5aeca0a81b896ea000a84
Saman Saadi <saman@saadi>
2017-02-24 01:08:09 -0500

    Issue #3 add getEntity to Character class.

--- src/ca/concordia/soen6441/d20/character/Player.java
+++ src/ca/concordia/soen6441/d20/character/Player.java
@@ -2,11 +2,14 @@ package ca.concordia.soen6441.d20.character;
 import java.util.ArrayList;
 import java.util.List;
 
+import ca.concordia.soen6441.d20.gamemap.element.GameObjectEntity;
 import ca.concordia.soen6441.d20.item.Item;
 /**
  * this a Player class .
  * @author alvaro
  */
+
+//TODO Do we really need a Character class?
 public class Player extends Character {
 	private static final int BACKPACK_MAX_CAPACITY = 5;
 	private static final int ELEMENT_NOT_FOUND = -1;
@@ -27,6 +30,12 @@ public class Player extends Character {
 		
 		backpack.set(index, item);
 	}
+	
+	@Override
+	public GameObjectEntity getEntity() {
+		// TODO Auto-generated method stub
+		return null;
+	}	
 	
 	public void pickUp(Item newItem) {
 		if(backpack.size() >= BACKPACK_MAX_CAPACITY) return;
