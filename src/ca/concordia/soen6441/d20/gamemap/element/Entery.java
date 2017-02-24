9d439491fd45cbb021796e862aec4c8b4d9e0eb5
negar <negar@sadeghi>
2017-02-24 16:15:46 -0500

    Issue #5 Working with Entities

--- src/ca/concordia/soen6441/d20/gamemap/element/Entery.java
+++ src/ca/concordia/soen6441/d20/gamemap/element/Entery.java
@@ -1,14 +1,22 @@
 package ca.concordia.soen6441.d20.gamemap.element;
 
 public class Entery extends GameObject{
+	
+	private EntryEntity entity;
 
 	public  Entery(){
-		setTag("Enter");
+		getEntity.setTag("Enter");
 	}
 
 	@Override
 	public GameObjectEntity getEntity() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public void setEntity(EntryEntity entity){
+		
+		this.entity = entity;
 	}
 }
