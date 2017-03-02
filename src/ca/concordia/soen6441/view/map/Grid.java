f9b7b568fdd25b5b728fa7a2a4e87b534c02332c
Mostafa Amin <Mostafa_chita@yahoo.com>
2017-03-02 00:05:51 -0500

    refactor version 4

--- src/ca/concordia/soen6441/view/map/Grid.java
+++ src/ca/concordia/soen6441/view/map/Grid.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+import ca.concordia.soen6441.d20.item.Item;
+import ca.concordia.soen6441.d20.character.Character;
+
 /**
  * this a Grid class for elements in mapView
  * @author wmg
@@ -17,21 +20,52 @@ public class Grid extends JButton implements ActionListener{
 	private ImageIcon icon;
 	private MapEditor mapEditor;
 	private String tag;
+	private Character character;
+	private Item item;
 	
 	public Grid(MapEditor mapEditor,String tag)
 	{
 		this.mapEditor = mapEditor;
 		this.setTag(tag);
-		
+		setCharacter(null);
+		setItem(null);
 		iconHandler();
 	}
-
+	
+	public Grid(Character character,MapEditor mapEditor,String tag) {
+		this.mapEditor = mapEditor;
+		this.setTag(tag);
+		setCharacter(character);
+		this.setItem(null);
+		iconHandler();
+	}
+	
+	public Grid(Item character,MapEditor mapEditor,String tag) {
+		this.mapEditor = mapEditor;
+		this.setTag(tag);
+		setItem(item);
+		setCharacter(null);
+		iconHandler();
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getID() == 1001){
+
 			setIcon(mapEditor.getCurrentPointer());
 			this.setTag(mapEditor.getTag());
+			if(mapEditor.getTag().equals("Player")){
+				setCharacter(mapEditor.getCharacter());
+				mapEditor.iconButtons[6].setVisible(false);
+				mapEditor.setCurrentPointer(mapEditor.images[2]);
+				mapEditor.setTag("Ground");
+			}
+			if(mapEditor.getTag().equals("Item")){
+				setItem(mapEditor.getItem());
+				mapEditor.iconButtons[5].setVisible(false);
+				mapEditor.setCurrentPointer(mapEditor.images[2]);
+				mapEditor.setTag("Ground");
+			}
 		}
 	}
 
@@ -70,6 +104,40 @@ public class Grid extends JButton implements ActionListener{
 		}else if (tag == "Exit"){
 			icon = new ImageIcon("5.png");
 			setIcon(icon);
+		}else if (tag == "Item"){
+			icon = new ImageIcon("6.png");
+			setIcon(icon);
+		}else if (tag == "Player"){
+			icon = new ImageIcon("7.png");
+			setIcon(icon);
 		}
+	}
+
+	/**
+	 * @return the character
+	 */
+	public Character getCharacter() {
+		return character;
+	}
+
+	/**
+	 * @param character the character to set
+	 */
+	public void setCharacter(Character character) {
+		this.character = character;
+	}
+
+	/**
+	 * @return the item
+	 */
+	public Item getItem() {
+		return item;
+	}
+
+	/**
+	 * @param item the item to set
+	 */
+	public void setItem(Item item) {
+		this.item = item;
 	}
 }
