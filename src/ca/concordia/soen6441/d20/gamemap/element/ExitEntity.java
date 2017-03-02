555f38b754da53f355768de222c9d46893bab22c
negar <negar@sadeghi>
2017-03-02 15:16:36 -0500

    Issue #7

--- src/ca/concordia/soen6441/d20/gamemap/element/ExitEntity.java
+++ src/ca/concordia/soen6441/d20/gamemap/element/ExitEntity.java
@@ -17,7 +17,9 @@ public class ExitEntity extends GameObjectEntity implements Serializable {
 	public ExitEntity() {
 		super();
 	}
-	
+	/**
+	 * @return a game object as exit for the game map
+	 */
 	public GameObject createModel(){
 		
 		return new Exit(this);
