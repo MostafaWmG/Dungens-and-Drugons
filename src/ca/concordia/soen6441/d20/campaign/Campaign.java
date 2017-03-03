d8afd53e40b317b892591f5d88b28d82194174c7
Saman Saadi <saman@saadi>
2017-03-02 19:03:15 -0500

    Issue #3 Support saving and loading campaigns.

--- src/ca/concordia/soen6441/d20/campaign/Campaign.java
+++ src/ca/concordia/soen6441/d20/campaign/Campaign.java
@@ -4,17 +4,42 @@ import java.util.LinkedList;
 import java.util.List;
 
 import ca.concordia.soen6441.d20.gamemap.GameMap;
+import ca.concordia.soen6441.d20.gamemap.GameMapEntity;
 
 public class Campaign {
-
+	
+	CampaignEntity campaignEntity;	
 	private List<GameMap> campaign;
-	private String name;
 	
 	public Campaign(String name) {
+		initEmptyEntity();
 		setName(name);
 		setCampaign(new LinkedList<GameMap>());
 	}
-
+	
+	public Campaign(CampaignEntity e)
+	{
+		setCampaignEntity(e);
+		for (GameMapEntity gameMapEntity : e.getCampaign())
+			addMap(gameMapEntity.createModel(), false);
+	}
+			
+	private void initEmptyEntity()
+	{
+		setCampaignEntity(new CampaignEntity());
+	}
+	
+	public void addMap(GameMap map)
+	{
+		addMap(map, true);
+	}
+	
+	private void addMap(GameMap map, boolean saveEntity)
+	{
+		getCampaign().add(map);
+		if (saveEntity)
+			getCampaignEntity().getCampaign().add(map.getEntity());
+	}
 	
 	public void show(){
 		for(int i = 0 ; i < getCampaign().size(); i ++){
@@ -41,7 +66,7 @@ public class Campaign {
 	 * @return the name
 	 */
 	public String getName() {
-		return name;
+		return getCampaignEntity().getName();
 	}
 
 
@@ -49,6 +74,27 @@ public class Campaign {
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
-		this.name = name;
+		getCampaignEntity().setName(name);
 	}
+
+	/**
+	 * @return the campaignEntity
+	 */
+	public CampaignEntity getCampaignEntity() {
+		return campaignEntity;
+	}
+
+	/**
+	 * @param campaignEntity the campaignEntity to set
+	 */
+	public void setCampaignEntity(CampaignEntity campaignEntity) {
+		this.campaignEntity = campaignEntity;
+	}
+
+	/**
+	 * @param campaign the campaign to set
+	 */
+	public void setCampaign(List<GameMap> campaign) {
+		this.campaign = campaign;
+	}		
 }
