38402f5ff8c82d1afd0cc70131572e18e69e48c0
Mostafa Amin <Mostafa_chita@yahoo.com>
2017-03-21 04:01:40 -0400

    Issue#24

---
+++ src/test/ca/concordia/soen6441/d20/item/TestSuiteItem.java
@@ -0,0 +1,11 @@
+package test.ca.concordia.soen6441.d20.item;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CanNotWearMoreThanOneTest.class,ChestTest.class,ItemWearTest.class,TestItem.class})
+public class TestSuiteItem {
+
+}
