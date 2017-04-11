c5db5ac66fe94ead6a62b93bb26409b606400b1c
negar <negar@sadeghi>
2017-04-11 07:02:17 -0400

    Issue #39

--- src/test/ca/concordia/soen6441/d20/common/LocationTest.java
+++ src/test/ca/concordia/soen6441/d20/common/LocationTest.java
@@ -13,31 +13,44 @@ import ca.concordia.soen6441.d20.common.Location;
 public class LocationTest {
 	private Location location;
 	
+	/**
+	 * This method creates a location object before each test case runs
+	 */
 	@Before
 	public void setUp() {
 		location = new Location(1,2);
 	}
 
+	/**
+	 * This test case checks that the location object is correctly created location (1,2)
+	 */
 	@Test
 	public void createLocation() {
 		assertEquals(1, location.getX());
 		assertEquals(2, location.getY());
 	}
-	
+	/**
+	 * This test case checks that two location objects that have the same coordinate
+	 * are actually equal
+	 */
 	@Test
 	public void twoEqualLocationsShouldBeEquals() {
 		Location otherLocation = new Location(1,2);
 		
 		assertEquals(location, otherLocation);
 	}
-	
+	/**
+	 * This test case is used to show that no different location objects are equal
+	 */
 	@Test
 	public void twoDifferentLocationsShouldNotBeEqual() {
 		Location otherLocation = new Location(2,1);
 		
 		assertNotEquals(location, otherLocation);
 	}
-	
+	/**
+	 * This test case checks the equality of the expected hash code and the test hash code
+	 */
 	@Test
 	public void hashCodeShouldBeEqualToHashCodeOfXAndYConcatenatedASString() {
 		int expectedHashCode = String.format("%d:%d", 1,2).hashCode();
